dcd393d6e8df5fdf43055136ba76cbde1daad02d
Sotirios Moschos <sotiriosmos@gmail.com>
2026-07-17 18:22:38 +0300

    Add: design-word-search-data-structure - submission-7

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-7.py
@@ -0,0 +1,30 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for index, char in enumerate(word):
+            if char == '.':
+                for child in curr.children:
+                    if self.search(word[:index] + child + word[index + 1:]):
+                        return True
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return curr.word

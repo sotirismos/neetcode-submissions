bdbc2ffd6a3d7ad41a68ad3eecc1146c1b229854
Sotirios Moschos <sotiriosmos@gmail.com>
2026-07-30 14:10:46 +0300

    Add: jump-game - submission-11

---
+++ Data Structures & Algorithms/jump-game/submission-11.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) - 1
+        for index in range(len(nums) - 2, -1, -1):
+            if index + nums[index] >= goal:
+                goal = index
+        return goal == 0 
